55149446695a95e4040c791f8c8800e72fc18d31
Hanish <dm281972@gmail.com>
2024-02-06 17:28:27 +0530

    booking page nav bar updated not fully

--- src/components/BookingNavBar.vue
+++ src/components/BookingNavBar.vue
@@ -1,23 +1,23 @@
 <template>
-<IonFooter class="bg-transparent shadow-none ion-padding">
-    <div class="w-fit max-w-sm mx-auto rounded-full white z-10 shadow-md p-0">
-        <div class="w-full flex justify-between items-center gap-4 py-1 px-2">
+<IonFooter class="bg-transparent shadow-none">
+    <div class="w-fit mx-auto white z-10  p-0">
+        <div class="w-full flex justify-between items-center gap-6 py-2">
 
-        <router-link :to="{name: 'active'}" class="flex justify-center items-center px-2  gap-2 rounded-full stroke-blue-900">
-            <div class="nav-link-text flex justify-center items-center bg-blue-800 rounded-full py-2 px-4 m-0">
-                <p class="text-[10px] font-extrabold text-center text-white">ACTIVE</p>
+        <router-link :to="{name: 'active'}" class="flex justify-center items-center rounded-full shadow-md">
+            <div class="flex justify-center items-center bg-blue-800 rounded-full py-2 px-4 m-0">
+                <p class="font-semibold text-center text-white">ACTIVE</p>
             </div>
         </router-link>
 
-        <router-link :to="{name: 'completed'}" class="flex justify-center items-center px-2 gap-2 rounded-full stroke-white">
-            <div class=" flex justify-center items-center bg-white rounded-full ">
-                <p class="text-[10px] font-extrabold text-center text-blue-800">COMPLETED</p>
+        <router-link :to="{name: 'completed'}" class="flex justify-center items-center  rounded-full shadow-md ">
+            <div class="flex justify-center items-center bg-white rounded-full py-2 px-4 m-0">
+                <p class="font-semibold text-center text-blue-800">COMPLETED</p>
             </div>
         </router-link>
 
-        <router-link :to="{name: 'cancelled'}" class="flex justify-center items-center px-2 gap-2 rounded-full stroke-white">
-            <div class=" flex justify-center items-center bg-white rounded-full ">
-                <p class="text-[10px] font-extrabold text-center text-blue-800">CANCELLED</p>
+        <router-link :to="{name: 'cancelled'}" class="flex justify-center items-center  rounded-full shadow-md">
+            <div class=" flex justify-center items-center bg-white rounded-full py-2 px-4 m-0">
+                <p class="font-semibold text-center text-blue-800">CANCELLED</p>
             </div>
         </router-link> 
         </div>
