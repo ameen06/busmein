a193f967ad9c07991dd53755c593a6af5fa2ed87
Hanish <dm281972@gmail.com>
2024-02-07 22:08:06 +0530

    booking page nav bar text-color edited , successful page nav changed to router-link

--- src/views/booking/BookingSuccessPage.vue
+++ src/views/booking/BookingSuccessPage.vue
@@ -9,9 +9,11 @@
             <p class="max-w-xs text-sm text-gray-500 font-[400] px-7">the details has been send to</p>
             <p class="max-w-xs text-sm text-gray-900 font-[700] px-7 mb-7">+91-8375982542</p>
         </div>
-        <div class="absolute top-[85%] max-w-sm bg-blue-800 px-16 py-2 rounded-full mx-auto"> 
-          <p class="text-white">Go To Booking Details</p>
-        </div>
+       <router-link :to="{}" class="absolute top-[85%]">
+          <div class=" bg-blue-800 px-16 py-2 rounded-full mx-auto"> 
+            <p class="text-white">Go To Booking Details</p>
+          </div>
+        </router-link>
        </div>
     </ion-content>
   </ion-page>
